0a3dc02b2cb4f57f66181cd7714610df8ec129aa
Kyelle Bantog <bantogk@sheridancollege.ca>
2023-11-13 14:13:15 -0500

    Initial Commit

---
+++ BookCollect/BookCollectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookCollectApp.swift
+//  BookCollect
+//
+//  Created by Kyelle on 2023-11-13.
+//
+
+import SwiftUI
+
+@main
+struct BookCollectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

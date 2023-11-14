50d2c1d74d6ac7f97922daf9c6f6befeabc87752
Melissa Munoz <munozme@sheridancollege.ca>
2023-11-14 14:47:58 -0500

    Added Book class and FireDBHelper class

---
+++ BookCollect/Data/FireDBHelper.swift
@@ -0,0 +1,8 @@
+//
+//  FireDBHelper.swift
+//  BookCollect
+//
+//  Created by Eli Munoz on 2023-11-14.
+//
+
+import Foundation
---
+++ BookCollect/Models/Book.swift
@@ -0,0 +1,8 @@
+//
+//  Book.swift
+//  BookCollect
+//
+//  Created by Eli Munoz on 2023-11-14.
+//
+
+import Foundation

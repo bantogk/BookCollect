5518aefc55dfd2e5d37756a19985e5b157e25412
Melissa Munoz <munozme@sheridancollege.ca>
2023-12-10 12:52:49 -0500

    Added alert in MapUI

--- BookCollect/Views/Map/MapView.swift
+++ BookCollect/Views/Map/MapView.swift
@@ -1,4 +1,5 @@
 // Melissa Munoz / Eli - 991642239
+//references: https://youtu.be/WTzBKOe7MmU?si=OMozbW-os4O_EgzH
 
 import SwiftUI
 import MapKit
@@ -9,6 +10,8 @@ struct MapView: View {
     @EnvironmentObject var router : TabRouter
     @EnvironmentObject var locationHelper : LocationHelper
     
+    @State private var search: String = ""
+    
     @State private var locations: [Location] = [Location]()
     
     @State private var isSheetPresented: Bool = false
@@ -18,39 +21,67 @@ struct MapView: View {
         ZStack{
             VStack{
                 
-                Text(self.locationHelper.currentLocation?.coordinate.longitude.description ?? "NA")
-                    .bold()
-                    .foregroundColor(.white)
-                Text(self.locationHelper.currentLocation?.coordinate.latitude.description ?? "NA")
-                    .bold()
-                    .foregroundColor(.white)
-                ZStack(alignment: .bottomTrailing){
-                    BookMap(locations: locations).environmentObject(self.locationHelper)
+                ZStack(alignment: .top) {
                     
+                    BookMap(locations: locations).environmentObject(self.locationHelper)
                         .sheet(isPresented: $isSheetPresented) {
-                            SheetView(locations: locations)
+                            SheetView(locations: self.locations)
                         }//sheet
                     
-                    Button {
-                        self.isSheetPresented = true
-                    } label: {
-                        Image(systemName: "magnifyingglass")
-                            .font(.title.weight(.semibold))
-                            .padding()
-                            .background(Color.pink)
-                            .foregroundColor(.white)
-                            .clipShape(Circle())
-                    }//Button
-                    .padding(.bottom,80)
-                    .padding(.trailing, 20)
+                    TextField("Search For Nearby Locations", text: $search, onEditingChanged: { _ in })
+                    {
+                        // commit
+                        self.getNearByLocations(search:self.search)
+                    }.textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                        .offset(y: 44)
                     
-                }//ZStack
+                }
             }//VStack
+            .onAppear(){
+                self.getNearByLocations(search:"Book")
+                
+            }
         }//ZStack
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(.yellow)
         .clipped()
     }//body
+
+    
+    private func getNearByLocations(search : String) {
+        
+        //MkLocalSearch vs Geocoding
+        //Geocoding recommendations based off coordinations, but userQuery, Mklocalsearch is more common to use
+        //A utility object for initiating map-based searches and processing the results.
+        
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = search
+        
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+                if let response = response {
+                    
+                    //get the locations
+                    let mapItems = response.mapItems
+                    
+                    //now we can create an array of locations
+                    self.locations = mapItems.map {
+                        Location(placemark: $0.placemark)
+                    }//self.locations
+                    
+                    //ensure to present Sheet
+                    self.isSheetPresented = true
+                    
+                    
+                }//if/else
+            
+        }//search.start
+        
+    }//getNearBy
+    
+    
+  
 }//Struct
 
 //#Preview {
